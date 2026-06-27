b9276166f0f7615526593b4242af3bae0f071f3c
Piyush Raj <piyush30450@gmail.com>
2026-06-28 01:27:02 +0530

    Add user entity and registration DTO

--- src/main/java/com/piyush/aios/ai_os/dto/RegisterRequest.java
+++ src/main/java/com/piyush/aios/ai_os/dto/RegisterRequest.java
@@ -1,5 +1,18 @@
 package com.piyush.aios.ai_os.dto;
 
-public class RegisterRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
+public class RegisterRequest {
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
